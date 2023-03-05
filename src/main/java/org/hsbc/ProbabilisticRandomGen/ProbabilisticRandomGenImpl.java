5a83370ea51ad309983b160795392e337b68152e
inadtest <inad45@gmail.com>
2023-03-05 01:24:01 -0500

    Initial probabilist random gen code

--- src/main/java/org/hsbc/ProbabilisticRandomGen/ProbabilisticRandomGenImpl.java
+++ src/main/java/org/hsbc/ProbabilisticRandomGen/ProbabilisticRandomGenImpl.java
@@ -1,42 +1,27 @@
 package org.hsbc.ProbabilisticRandomGen;
 
-import io.vavr.Tuple2;
-import org.hsbc.ProbabilisticRandomGen.ProbabilisticRandomGen;
-
-import java.util.List;
 import java.util.Random;
 
-import static java.util.Collections.emptyList;
-import static java.util.Optional.ofNullable;
-import static java.util.stream.Collectors.toList;
-
 public class ProbabilisticRandomGenImpl implements ProbabilisticRandomGen {
-    List<NumAndProbability> numAndProbabilities;
+    private final int[] numberList;
+    private final float[] probabilityList;
     private final Random random;
 
-    public ProbabilisticRandomGenImpl(List<NumAndProbability> numAndProbabilities) {
-        this.numAndProbabilities = List.copyOf(numAndProbabilities);
-        this.random = new Random();
-    }
-
-    public ProbabilisticRandomGenImpl of(Tuple2<Integer, Float>... numAndProbabilities) {
-        return new ProbabilisticRandomGenImpl(ofNullable(numAndProbabilities)
-                .map(List::of)
-                .orElse(emptyList())
-                .stream()
-                .map(tuple -> new NumAndProbability(tuple._1(), tuple._2()))
-                .collect(toList()));
+    public ProbabilisticRandomGenImpl(int[] numbers, float[] probabilities, Random random) {
+        this.probabilityList = probabilities;
+        this.numberList = numbers;
+        this.random = random;
     }
 
     @Override
     public int nextFromSample() {
         float sum = 0;
         float rand = random.nextFloat();
-        for(NumAndProbability n : numAndProbabilities) {
-            sum += n.getProbabilityOfSample();
+        for(int i = 0; i < probabilityList.length; i++) {
+            sum += probabilityList[i];
             if(rand < sum)
-                return n.getNumber();
+                return numberList[i];
         }
-        return numAndProbabilities.get(numAndProbabilities.size()-1).getNumber();
+        return 0;
     }
 }
